39d520d0ea75de87e17ddd07ba91d11f96f10535
GLYSATVIK <codeforcessatvik@gmail.com>
2025-09-09 15:01:59 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,6 +2,8 @@ from ftplib import FTP
 
 def ftp_test():
     try:
+        with open("test.txt","w") as f: f.write("Hello FTP test")
+
         f = FTP("ftp.dlptest.com"); f.login("dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu")
         f.storbinary("STOR test.txt", open("test.txt","rb"))
         f.retrbinary("RETR test.txt", open("out.txt","wb").write)
